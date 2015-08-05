bbfd0862bd7c2117c59f28199dc7fc885f2079bb
Manuel Gamallo <mgamallo@gmail.com>
2015-08-05 23:03:56 +0200

    Commit inicial

---
+++ Clock/src/clock/ClockServlet.java
@@ -0,0 +1,13 @@
+package clock;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class ClockServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hola Mundo!!");
+	}
+}
